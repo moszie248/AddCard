baa7ff352a541d3081aab7a5e6a521b4be93a649
Moshi <moszie248@hotmail.com>
2018-07-10 13:55:26 +0700

    Add Card Trello Finish
    but sometime cant add,i have no idea.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Manatee.Trello;
@@ -15,20 +16,34 @@ namespace FunctionCreateTask
 {
     public class Program
     {
-        static void Main()
+         public static void Main()
         {   
-            Console.WriteLine("Creating a card ...");
+ 
+        AddCard();
+
+        }
+        private static async Task AddCard()
+        {
+            Console.WriteLine("Inside");
              
             TrelloAuthorization.Default.AppKey = "109fe6fc4bf193d45e7730f7055440b0";
             TrelloAuthorization.Default.UserToken = "d6af304d5c424d0d8862b03781c32ddd8adf2ea7aa5ff97bba45b230209cf5a1";
-            ITrelloFactory factory = new TrelloFactory();
-            //var backlog = factory.List("[5b40363293193091c1ba5014]");
-            //var board = factory.Board("[5b40363293193091c1ba5013]");
-            var list = factory.List("5b40363293193091c1ba5014");
-            var card = list.Cards.Add("a new card");
 
-            Console.WriteLine("Finished");
+            ITrelloFactory factory = new TrelloFactory(); 
+            var List = factory.List("5b40363293193091c1ba5014");
+ 
+        
+            Console.WriteLine("Creating a Card .......");
+            var card = List.Cards.Add("This is A NEW Card");
+            Console.WriteLine("Create Finished");
+            await List.Refresh();
+         
+
+
+            return ;
+
         }
+
     }
 }
     
